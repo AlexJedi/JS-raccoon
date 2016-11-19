7e627cb5559895d86c18e966d9b3f457aad2dde4
elliepooh <Ellie-Pooh@yandex.ru>
2016-11-19 19:56:22 +0300

    Update 19.11.2016
    JS update

--- js/script.js
+++ js/script.js
@@ -1,25 +1,51 @@
 'use strict';
 
 function readyScroll() {
-  window.scrollTo(0 , document.body.scrollHeight);
-  document.body.style.overflow = 'hidden';
+    window.scrollTo(0, document.body.scrollHeight);
+    document.body.style.overflow = 'hidden';
 }
 
 function getCoordY(elem) {
-  let box = elem.getBoundingClientRect();
-  return box.top + pageYOffset;
+    let box = elem.getBoundingClientRect();
+    return box.top + pageYOffset;
 }
 
 function levelScroll() {
-  let lvl = document.getElementsById('firstlvl');
-  window.scrollTo(0 , getCoordY(lvl));
+    let lvl = document.getElementById('firstlvl');
+    window.scrollTo(0, getCoordY(lvl));
+}
+
+function addClick(elementId, func) {
+    let elem = document.getElementById(elementId);
+    elem.addEventListener('click', func);
 }
 
 function startFirstLevel() {
-  levelScroll();
+    levelScroll();
 }
 
 document.addEventListener('DOMContentLoaded', readyScroll);
 
-let playBtn = document.getElementById('js-play');
-playBtn.addEventListener('click', startFirstLevel);
+addClick('js-play', startFirstLevel);
+addClick('js-back', readyScroll);
+
+let timeCounter = document.getElementById('js-time'),
+    timeCounterBlock = timeCounter.parentElement;
+
+function clearField() {
+    let timer = 2,
+        timerId = setInterval(function() {
+            timeCounter.innerHTML = timer;
+            if (timer == -1) {
+              clearInterval(timerId);
+              timeCounterBlock.style.display = 'none';
+            }
+            timer--;
+        }, 700);
+}
+clearField();
+
+let canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d");
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
